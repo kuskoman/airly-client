5cb77f9346f4493e6dadf942f039b1ee09e4fee7
kkm <kubasurdej@gmail.com>
2019-10-08 18:51:38 +0200

    Add AirlyClient object with headers method

---
+++ lib/airly-client.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+
+class AirlyClient
+    def initialize(api_key, base_url = "https://airapi.airly.eu/v2/")
+        @api_key = api_key
+        @base_url = base_url
+    end
+
+    private
+
+        def headers
+            {
+                'Accept' => 'application/json',
+                'Accept-Language' => 'en',
+                'apikey' => @api_key
+            }
+        end
+end
