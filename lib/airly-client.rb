114c8de391669c011c9cc7b0e1d71ad46e2b4e00
kkm <kubasurdej@gmail.com>
2019-10-08 20:13:15 +0200

    Make fetch method working

--- lib/airly-client.rb
+++ lib/airly-client.rb
@@ -1,9 +1,20 @@
 require 'httparty'
+require 'uri'
 
 class AirlyClient
     def initialize(api_key, base_url = "https://airapi.airly.eu/v2/")
         @api_key = api_key
         @base_url = base_url
+    end
+
+    def nearest(latitude, longtitude, maxDistanceKM: 5, maxResults: 1)
+        res = fetch("installations/nearest",
+            {
+                "lat" => latitude,
+                "lng" => longtitude,
+                "maxDistanceKM" => maxDistanceKM,
+                "maxResults" => maxResults
+            })
     end
 
     private
@@ -16,10 +27,18 @@ class AirlyClient
             }
         end
 
-        def fetch(resource)
+        def fetch(resource, params = {})
             HTTParty.get(
-                base_url + resource,
-                headers: headers 
+                @base_url + resource + stringify_params(params),
+                headers: headers
             )
+        end
+
+        def stringify_params(params)
+            if params.nil?
+                ""
+            else
+                "?" + URI.encode_www_form(params)
+            end
         end
 end
